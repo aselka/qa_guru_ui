e64316081d450ba2806ae6f045e0fdcef3158332
aselka <bis-aselya@yandex.ru>
2023-04-08 10:30:58 +0400

    new commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,9 @@
 import pytest
-import os
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
+
 from demoqa_tests.utils import attach
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -23,21 +17,20 @@ def open_browser():
             "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
+    browser.config.window_width = 1600
+    browser.config.window_height = 1440
 
-    yield
+    yield browser
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
+    browser.quit()
